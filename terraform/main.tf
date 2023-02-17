8400fb91b9bd9f6b942079e2c6a6da29f3242071
Stefan Weise <stefan.weise@19bytes.de>
2023-02-17 16:06:54 +0100

    Add my ssh key

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "hcloud_server" "node1" {
   image       = "ubuntu-22.04"
   server_type = "cx11"
   # pre configured in hetzner ui right now
-  ssh_keys = ["julian"]
+  ssh_keys = ["julian","stefan"]
   location = "nbg1"
   public_net {
     ipv4_enabled = true
