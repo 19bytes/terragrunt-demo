ac6be6ba5d7d8a075bf6280f601fba4c2abf0c24
Julian Paul <jolle@julian-paul.de>
2023-03-14 08:56:40 +0100

    make resource name unique

--- hcloud/server/main.tf
+++ hcloud/server/main.tf
@@ -2,7 +2,7 @@ data "hcloud_placement_group" "pg" {
   name = var.server_placement_group
 }
 
-resource "hcloud_server" "vm" {
+resource "hcloud_server" "vm-$${var.environment}" {
   name        = var.server_name
   image       = var.server_image
   server_type = var.server_type
